71812c4d759a5d81d3b4d6e798e7049a86a996c2
Matt Birkel <mbirkel91@gmail.com>
2023-02-15 01:30:07 -0600

    Tuning

--- DriverStation/DriverStation.py
+++ DriverStation/DriverStation.py
@@ -219,7 +219,7 @@ def arcadeDrive(yIn, rIn):
     yExpConst = 1.5  # exponential growth coefficient of the Y-axis translation -- should be between 1.0-4.0
     yEndpoint = 127  # maximum/minumum (+/-) for the Y-axis translation
 
-    rExpConst = 1.5  # exponential growth coefficient of the rotation -- should be between 1.0-4.0
+    rExpConst = 3.0  # exponential growth coefficient of the rotation -- should be between 1.0-4.0
     rEndpoint = 70   # maximum/minimum (+/-) for the rotation
 
     endExpConst = 1.44 # don't change this unless you've really looked over the math
